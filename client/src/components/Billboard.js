05ef4103e3c54c06b0959f9581c705cb8e79fd9b
Dylan Pople <dylanpople@Dylans-MacBook-Pro.local>
2017-09-08 16:04:26 -0700

    added components

---
+++ client/src/components/Billboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Song from 'Song';
+
+const Billboard = ({ songs, updateSong, deleteSong }) => (
+  <div className="row">
+    { songs.map( song =>
+    <Song
+      key={song.id}
+      {...song}
+      updateSong={updateSong}
+      deleteSong={deleteSong}
+    />
+  )}
+  </div>
+)
+export default Billboard
