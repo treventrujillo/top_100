3673d8125678f0a6e10c36887f90fbf7e95439f7
Dylan Pople <dylanpople@Dylans-MacBook-Pro.local>
2017-09-08 16:17:26 -0700

    worked on song.js

---
+++ client/src/components/Song.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Song = ({ id, name, updateSong, deleteSong }) => (
+  <div className="col s12">
+    <div className="col m8">
+      <div className="center">
+        {name}
+      </div>
+    </div>
+    <div className="col m2">
+      <input
+        id={``}
+
+)
+
+export default Song
